f6ec98fb1266d20e31dc0e1264123e5caef85ea3
X-TECH CORP <zabsonrejacques30@gmail.com>
2025-01-14 18:54:29 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "8064640180:AAGPv111K2i9y0UBc6xZZ-e5vpv8kOef4C8", // Token bot Telegram
+  OWNER_ID: "7192957519", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
